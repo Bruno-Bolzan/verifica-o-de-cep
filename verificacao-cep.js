cdea3404c70259fb699dcc0ac598354437642111
Bruno-Bolzan <38505200+Bruno-Bolzan@users.noreply.github.com>
2018-04-18 16:07:29 -0300

    Update verificacao-cep.js

--- verificacao-cep.js
+++ verificacao-cep.js
@@ -13,11 +13,11 @@ if(cep.length == 8){
                 $.get(
                     'https://viacep.com.br/ws/' + estado + '/' + cidade + '/' + rua + '/json/',
                     function(response){
-						var x = 0;
-						while(x < response.length){
-							console.log(response[x].cep + '\nLogradouro: ' + response[x].logradouro + '\nBairro: ' + response[x].bairro + '\nLocalidade: ' + response[x].localidade  + '\nEstado: ' + response[x].uf  + '\nCodigo IBGE: ' + response[x].ibge + '\n\n' );
-							x++;
-						}
+				var x = 0;
+				while(x < response.length){
+					console.log(response[x].cep + '\nLogradouro: ' + response[x].logradouro + '\nBairro: ' + response[x].bairro + '\nLocalidade: ' + response[x].localidade  + '\nEstado: ' + response[x].uf  + '\nCodigo IBGE: ' + response[x].ibge + '\n\n' );
+					x++;
+				}
                     }
                 )
             }else{
